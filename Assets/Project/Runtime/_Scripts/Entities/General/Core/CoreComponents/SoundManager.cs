09dd9354e934743baf1efd8de0ae1cb53f1e3dda
VinzRoosen <vinz.roosen@skynet.be>
2023-09-11 18:23:05 +0200

    Added soundManager

---
+++ Assets/Project/Runtime/_Scripts/Entities/General/Core/CoreComponents/SoundManager.cs
@@ -0,0 +1,25 @@
+using Project.Entity.CoreSystem;
+using UnityEngine;
+
+namespace Project
+{
+    public class SoundManager : CoreComponent
+    {
+        public void PlayAudio(AudioSource audioSource)
+        {
+            if (audioSource != null) { audioSource.Play(); }
+            else { Debug.LogWarning("AudioSource is null"); }
+        }
+
+        //Test
+        [SerializeField] GameObject defaultAudio;
+        [ContextMenu("Play audio Test")]
+        public void PlayAudioTest()
+        {
+            GameObject audioObject = Instantiate(defaultAudio);
+            AudioSource audioSource = audioObject.GetComponent<AudioSource>();
+            PlayAudio(audioSource);
+            Destroy(audioObject, audioSource.clip.length * 2);
+        }
+    }
+}
