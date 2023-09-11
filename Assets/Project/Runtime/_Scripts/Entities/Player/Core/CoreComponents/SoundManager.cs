04d2ab9d78967766a6d8863331496fac2d88d9ed
VinzRoosen <vinz.roosen@skynet.be>
2023-09-11 14:13:16 +0200

    Added SoundManager

---
+++ Assets/Project/Runtime/_Scripts/Entities/Player/Core/CoreComponents/SoundManager.cs
@@ -0,0 +1,26 @@
+using Project.Entity.Player.Core;
+using UnityEngine;
+
+namespace Project
+{
+    public class SoundManager : CoreComponent
+    {
+        public void PlayAudio(AudioSource audioSource)
+        {
+            if (audioSource != null) { audioSource.Play(); }
+            else { Debug.LogWarning("AudioSource is null"); }
+        }
+
+
+        //Test
+        [SerializeField] GameObject defaultAudio;
+        [ContextMenu("Play audio Test")]
+        public void PlayAudioTest()
+        {
+            GameObject audioObject = Instantiate(defaultAudio);
+            AudioSource audioSource = audioObject.GetComponent<AudioSource>();
+            PlayAudio(audioSource);
+            Destroy(audioObject, audioSource.clip.length * 2); //*2 omdat de source anders voor het einde van de clip destroyed werd. Ik weet ook niet waarom.
+        }
+    }
+}
